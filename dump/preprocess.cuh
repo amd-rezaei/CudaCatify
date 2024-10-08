84a50b10d207e25a3e026b872b1259b2a0d1ef9f
Amaad <ahmadrezaeiq@gmail.com>
2024-10-08 13:23:15 +0200

    removed dumps for yolov4

--- dump/preprocess.cuh
+++
@@ -1,7 +0,0 @@
-// preprocess.cuh
-#ifndef PREPROCESS_CUH
-#define PREPROCESS_CUH
-
-extern "C" void preprocess(float *d_input, unsigned char *image, int width, int height, int channels, int input_width, int input_height);
-
-#endif
--- dump/readme.txt
+++
@@ -1,3 +0,0 @@
-Link for models and dir:
-
-https://github.com/abuod0/Improved-Face-Detection-YOLOv5-Based/tree/main
